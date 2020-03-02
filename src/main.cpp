032ccfe2f9f7e36450a4798d6f303191c4e0d128
Stuart Irwin <stuin01@gmail.com>
2020-03-02 11:33:40 -0800

    Connected input arguents to program variable

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,15 @@ int main(int argc, char const *argv[])
 	Enviroment env;
 
 	if(argc > 1) {
+		sexpr args;
+		int i = 2;
+
+		//Copy arguments into env variable
+		while(i < argc)
+			args.push_back(env.read(argv[i++]));
+		env.set("args", cell(args, LIST));
+
+		//Run actual file
 		std::ifstream infile(argv[1]);
 		if(infile.good()) {
 			while(!infile.eof())
